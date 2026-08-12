5038d77271640ad3713efd0f9fecbf9272cfe8a6
David William <sluuckejoohn@gmail.com>
2026-08-12 17:08:53 -0300

    Bump to 1.6.0 for the Change profile banner feature

--- src/updater.py
+++ src/updater.py
@@ -22,7 +22,7 @@ import requests
 
 logger = logging.getLogger("lol-profiler-tool")
 
-APP_VERSION = "1.5.3"
+APP_VERSION = "1.6.0"
 GITHUB_REPO = "sluucke/lol-profiler-tool"
 
 _RELEASES_API = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
